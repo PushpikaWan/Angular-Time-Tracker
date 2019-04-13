beb8a4890121ef7d613b818ee77055f82d43114c
PushpikaWan <lucefer41@gmail.com>
2019-04-13 12:59:49 +0530

    material date and time pickers added

--- src/app/models/task.module.ts
+++ src/app/models/task.module.ts
@@ -1,12 +1,17 @@
 import { Timer } from "./timer.module";
 import { Project } from "./project.module";
 import { Tag } from "./tag.module";
+import { Time } from "./time.module";
 
 export class Task{
     id?: String;
     description: String;
-    project?: Project
+    project: Project
     tag?: Tag
     time?: Timer;
-    state?: String; //replace with enum
+    state?: String; 
+    date? : Date;
+    startingTime? : Time;
+    endingTime? : Time;
+    //replace with enum
 }
---
+++ src/app/models/time.module.ts
@@ -0,0 +1,6 @@
+
+export class Time{
+
+    hours: number;
+    mintues: number;
+}
