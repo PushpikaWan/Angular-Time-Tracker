c2201647a087aa5898aab7a1f69d3be2e120f4fc
PushpikaWan <lucefer41@gmail.com>
2019-04-17 00:50:51 +0530

    task grouping initiated

--- src/app/task-page/task-page.component.ts
+++ src/app/task-page/task-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task.module';
+import { TaskService } from '../services/task.service';
+import { groupBy } from 'rxjs/operators';
 
 @Component({
   selector: 'app-task-page',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskPageComponent implements OnInit {
 
-  constructor() { }
+  taskListByDate: any; //todo add relevent type for this
+  
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.taskService.taskListChanged.subscribe(
+      (taskData: Task[]) => {
+          this.createDateSpecificList(taskData);
+          // this.taskListByDate=taskData;
+          // console.log(this.taskListByDate);
+      }
+    );
   }
+
+  private createDateSpecificList(taskData : Task[]) {
+    let result = taskData.map((item) => { 
+      return this.getPresnetableDate(item["date"]); 
+    });
+    console.log(result);
+
+    
+  }
+
+  private getPresnetableDate(date :Date): PresentableDate{
+    return {date:date.getDate(),month:date.getMonth(), year: date.getFullYear()};
+  }
+
+
+  // private groupBy = (xs) => {
+  //   return xs.reduce(
+  //     (rv, x) =>{
+  //     (rv[this.getPresnetableDate(x.date)] = rv[this.getPresnetableDate(x.date)] || []).push(x);
+  //     return rv;
+  //   }, {});
+  // };
+
+}
+
+export class PresentableDate{
+
+    date: number;
+    month: number;
+    year: number;
 
 }
